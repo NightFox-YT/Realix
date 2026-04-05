c6c66f9bf604bb74565f8dad50a4c5386a1eff25
MainFoxYT <goomarket040@gmail.com>
2026-04-05 11:30:26 +0200

    (v0.05) Release v2.0
    There were too few changes from the previous version, so I added file reading (improved reading from bootix) and kernel layout.

--- source/initrix.asm
+++ source/initrix.asm
@@ -1,5 +1,5 @@
 ; © Realix > Initrix
-; (04.04.26) v0.05
+; (05.04.26) v0.05
 ; ================
 
 ; Настройка компиляции
@@ -14,14 +14,13 @@ main:
     ; Получение номера диска, переданного из bootix
     mov [boot_drive], dl
 
-    ; "Инициализация..."
-    mov si, msg_init
+    mov si, msg_init  ; "Инициализация"
     call print
 
     ; Получение объёма доступной "нижней" памяти (до 640 КБ)
     clc
     int 12h
-    jc .memory_error
+    jc memory_error
     mov [memory_kb], ax
 
     ; Сохранение собранной информации (С сбросом сегмента на 0x0)
@@ -46,27 +45,44 @@ main:
 
     mov si, str_title
     call print
+
     mov si, str_memory
     call print
     mov ax, [memory_kb]
     call print_reg
     mov si, str_kb
     call print
+
     mov si, double_new_line
     call print
 
-    ; (Временно) "Эта версия Realix достигла конца работы"
-    mov si, msg_end
+    ; "Загрузка ядра..."
+    mov si, msg_loading
     call print
 
+    ; Загрузка ядра с диска
+    mov si, kernel_filename
+    mov ax, KERNEL_LOAD_SEGMENT
+    mov bx, KERNEL_LOAD_OFFSET
+    call load_file
+
+    ; Настройка сегментов и регистров под kernel
+    mov ax, KERNEL_LOAD_SEGMENT
+    mov ds, ax
+    mov es, ax
+    mov di, PCINFO_ADDR
+
+    ; Передача управления ядру
+    jmp KERNEL_LOAD_SEGMENT:KERNEL_LOAD_OFFSET
+
 ; Остановка CPU
-.halt:
+halt:
     cli
     hlt
     jmp $
 
 ; Ошибка памяти
-.memory_error:
+memory_error:
     mov si, err_memory
     jmp error_handler
 
@@ -84,25 +100,30 @@ error_handler:
 
 ; Подключение модулей
 %include 'kernel/print.asm'
-%include 'kernel/print_reg.asm'
+%include 'kernel/print_dec.asm'
 %include 'disk/read.asm'
+%include 'disk/fat12.asm'
 
 ; Сообщения
-msg_init:   db '[+] (Initrix) Initializing...', ENTER, 0
-msg_end:    db '[!] System halted. This version of Realix has ended its work.', ENTER, 0
-err_memory: db '[!] Memory error (int 12h)!', ENTER, 0
+msg_init:    db '[+] Initializing.', ENTER, 0
+msg_loading: db '[+] Loading kernel.', ENTER, 0
+err_memory:  db '[!] Memory error (int 12h)!', ENTER, 0
 
-; Строки загрузочного экрана и вспомогательные
+; Строки загрузочного экрана
 str_title:       db '     Realix v0.05', ENTER, '(C) NightFox developer', ENTER, ENTER, 0
 str_memory:      db 'Low Memory: ', 0
 str_kb:          db ' KB / 640 KB', 0
+
+; Вспомогательные строки
 char_beep:       db 0x07, 0
 new_line:        db ENTER, 0
 double_new_line: db ENTER, ENTER, 0
 
 ; Переменные
-boot_drive: db 0
-memory_kb:  dw 0
+kernel_filename: db 'KERNEL  BIN'
+boot_drive:      db 0
+memory_kb:       dw 0
 
-; Адрес структуры pc_info
+KERNEL_LOAD_SEGMENT equ 0x1000
+KERNEL_LOAD_OFFSET  equ 0x0000
 PCINFO_ADDR equ 0x0500
