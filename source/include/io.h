606ba07e291a85abadb268a4b75557ca4a1069a8
gnom2949 <sasasilaev27@gmail.com>
2026-06-18 20:13:58 +0500

    тут заголовки ядра, остальное в libc

--- source/include/io.h
+++ source/include/io.h
@@ -11,13 +11,13 @@ static inline void outb(uint16_t port, uint8_t val)
 static inline uint8_t inb(uint16_t port)
 {
 	uint8_t ret;
-	__asm__ volatile("inb %1, %0", : "=a"(ret) : "Nd"(port));
+	__asm__ volatile("inb %1, %0" : "=a"(ret) : "Nd"(port));
 	return ret;
 }
 
 static inline void outl(uint16_t port, uint32_t val)
 {
-	__asm__ volatile("inl %1, %0" : "a"(val), "Nd"(port));
+	__asm__ volatile("inl %1, %0" : : "a"(val), "Nd"(port));
 }
 
 static inline uint32_t inl(uint16_t port)
