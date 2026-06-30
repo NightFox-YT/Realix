17a2afba63546b67d3e339447ba26794dd99947c
oxxx1mif <pluginexteragram@gmail.com>
2026-06-30 15:07:50 +1000

    create crypto function blake3

---
+++ source/crypto/blake3/src/lib.rs
@@ -0,0 +1,8 @@
+// Copyright Gleb Obitotsky <https://github.com/oxxx1mif> 2026.
+//
+// License: GNU General Public License v3
+// You can find the license file in the project root.
+//
+// Implementation crypto version 0.1
+// The code was written for Realix.
+// 30 june 2026
